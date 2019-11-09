b60b1d57ebf929397df2f071b7a755f834d13f40
FoxtrotRei <52555476+FoxtrotRei@users.noreply.github.com>
2019-11-09 10:50:01 -0800

    Bug

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -26,6 +26,7 @@ public class PlayerMove : MonoBehaviour
         x = rotationModuleOn ? Input.GetAxis("Horizontal") : 0;
         y = rotationModuleOn ? Input.GetAxis("Vertical") : 0;
         drive = translationModuleOn ? Input.GetAxis("Drive") : 0;
+        Debug.Log(drive);
 
         if (drive != 0)
             isMoving = true;
