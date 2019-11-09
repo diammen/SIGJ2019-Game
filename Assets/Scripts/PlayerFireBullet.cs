50b64c3c0cf8c79a2bb13aa75f28f85f74b82a1e
FoxtrotRei <52555476+FoxtrotRei@users.noreply.github.com>
2019-11-09 10:28:14 -0800

    Added "Throwing"

---
+++ Assets/Scripts/PlayerFireBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFireBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawner;
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Hold down the button and charge up your shot.
+
+        //Release the button and fire
+        if (Input.GetButtonUp("Fire1"))
+        {
+            GameObject spawnedBullet = Instantiate(bullet, spawner.position, spawner.rotation);
+            spawnedBullet.GetComponent<Rigidbody>().velocity = spawner.transform.forward * bulletSpeed;
+        }
+
+    }
+}
