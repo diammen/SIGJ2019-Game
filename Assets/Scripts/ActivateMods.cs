0fbf7c39c0fdadbe7d44ed29976aa2eca7e83ff1
FoxtrotRei <52555476+FoxtrotRei@users.noreply.github.com>
2019-11-09 13:18:38 -0800

    Rei

--- Assets/Scripts/ActivateMods.cs
+++ Assets/Scripts/ActivateMods.cs
@@ -6,11 +6,14 @@ public class ActivateMods : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        
         if (collision.gameObject.tag == "Module")
         {
+            Debug.Log("Hit");
             if (collision.gameObject.name == "ActivateRotation")
             {
                 GetComponent<PlayerMove>().rotationModuleOn = true;
+
             }
             else if(collision.gameObject.name == "ActivateTranslation")
             {
