a5723df97d005fef220536e08b6f85fd26861db3
diammen <marcelnamdao@gmail.com>
2019-11-09 00:04:10 -0800

    basic movement and camera rotation

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTarget;
+    public float smoothTime;
+    public float rotationSpeed;
+
+    Vector3 offset;
+    Vector3 velocity;
+    float x;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Camera Horizontal");
+
+        offset = Quaternion.AngleAxis(x * rotationSpeed, Vector3.up) * offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, followTarget.position + offset, ref velocity, smoothTime);
+        transform.rotation = Quaternion.LookRotation(followTarget.position - transform.position, Vector3.up);
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float moveSpeed;
+    public float rotationSpeed;
+    public bool translationModuleOn;
+    public bool rotationModuleOn;
+
+    Rigidbody rb;
+    float x, y;
+    float drive;
+    bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = rotationModuleOn ? Input.GetAxis("Horizontal") : 0;
+        y = rotationModuleOn ? Input.GetAxis("Vertical") : 0;
+        drive = translationModuleOn ? Input.GetAxis("Drive") : 0;
+
+        if (drive != 0)
+            isMoving = true;
+        else
+            isMoving = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            Vector3 lookVec = new Vector3(x, 0, y);
+            Quaternion lookRot = rb.rotation;
+
+            if (lookVec != Vector3.zero)
+                lookRot = Quaternion.LookRotation(lookVec, rb.transform.up);
+
+            rb.AddForce(Camera.main.transform.forward * drive * moveSpeed, ForceMode.Impulse);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, lookRot, Time.deltaTime * rotationSpeed));
+        }
     }
 }
