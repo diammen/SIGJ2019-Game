3da9c87625c315a76c1a6d6b0a2ee8e98387016b
sdbaker <4412340+sdbaker@users.noreply.github.com>
2019-11-09 11:40:14 -0800

    add start and pause menus

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private GameObject[] pauseObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) { pause(); }
+
+    }
+
+    public void pause()
+    {
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+
+
+    void hidePaused()
+    {
+        foreach(GameObject g in pauseObjects){
+            g.SetActive(false);
+        }
+    }
+
+    private void showPaused()
+    {
+        foreach(GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void LoadLevel(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
