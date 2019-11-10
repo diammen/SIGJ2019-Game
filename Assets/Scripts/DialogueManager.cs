c7e9ba624aaab251189fab204ad2ef46d6df8a25
FoxtrotRei <52555476+FoxtrotRei@users.noreply.github.com>
2019-11-10 12:48:18 -0800

    Dialogue implemented

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public float lastingTime = 10f;
+    private float countTime = 0.0f;
+    public string[] displayText;
+    public bool timer = false;
+    public Text dialogue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,35 @@ public class DialogueManager : MonoBehaviour
     void Update()
     {
         
+        if (timer)
+        {
+            countTime += Time.deltaTime;
+            dialogue.text = displayText[0];
+            if (countTime >= lastingTime / 2)
+            {
+                dialogue.text = displayText[1];
+            }
+            if(countTime >= lastingTime)
+            {
+                countTime = 0.0f;
+                dialogue.text = " ";
+                timer = false;
+            }
+        }
+
+    }
+
+    public void setTextToShow(string one, string two)
+    {
+        displayText[0] = one;
+        displayText[1] = two;
+        timer = true;
+    }
+
+    void displayDialogue()
+    {
+        
+
+
     }
 }
