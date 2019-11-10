d483eb446189745420da0dbf461d0b23019e1479
FoxtrotRei <52555476+FoxtrotRei@users.noreply.github.com>
2019-11-09 19:17:10 -0800

    Living Room

---
+++ Assets/Scripts/LivingRoom/DropCouches.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCouches : MonoBehaviour
+{
+    public GameObject falseFloor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(falseFloor);
+        }
+    }
+
+
+}
